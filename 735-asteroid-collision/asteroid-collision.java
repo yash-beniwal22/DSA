1e30f68a20aaa0e0f994488ea9d1d78250869729
yash-beniwal22 <yashbeniwal2222@gmail.com>
2026-07-03 21:41:25 +0530

    Time: 4 ms (83.11%) | Memory: 46.9 MB (71.79%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            boolean destroyed = false;
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                if (stack.peek() < -asteroid) {
+                    stack.pop();
+                } else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+            if (!destroyed) {
+                stack.push(asteroid);
+            }
+        }
+        int[] ans = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+}

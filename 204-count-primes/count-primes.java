55f953e7be37006c8c2f3631ac0fe20cfa400316
yash-beniwal22 <yashbeniwal2222@gmail.com>
2026-07-09 23:01:49 +0530

    Time: 94 ms (81.36%) | Memory: 51 MB (87.16%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+        boolean[] isComposite = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!isComposite[i]) {
+                count++;
+                if ((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isComposite[j] = true;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
